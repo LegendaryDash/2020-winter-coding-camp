abebf2a2086466ae17b8de9116ba313939806b87
JooHo Lee <jooho7@yonsei.ac.kr>
2020-01-16 12:34:40 +0900

    Correct assertion typos

--- hw1/main.c
+++ hw1/main.c
@@ -20,20 +20,26 @@ int main()
 
 	int before = 2;
 	int new_data = 3;
+	
 	vector_append_after(&v1, before, new_data);
 
 	int where = 0;
 	int modifying_data = 88;
 	vector_modify(&v1, where, modifying_data);
 
+
 	int len = vector_size(&v1);
 	assert(len == 11);
-
+	
+		
 	assert(88 == vector_lookup(&v1, 0));
-	assert(1 == vector_lookup(&v1, 1));
-	assert(2 == vector_lookup(&v1, 3));
+	
+	for (int i = 1; i < before + 2; i++) {
+		assert(i == vector_lookup(&v1, i));
+	}
 
-	for (int i = 3; i < len; ++i) {
+	
+	for (int i = before + 2; i < len; ++i) {
 		assert(i-1 == vector_lookup(&v1, i));
 	}
 
@@ -41,18 +47,27 @@ int main()
 	vector_remove(&v1, target);
 
 	len = vector_size(&v1);
+
 	assert(len == 10);
-
+	
 	assert(88 == vector_lookup(&v1, 0));
-	assert(1 == vector_lookup(&v1, 1));
-	assert(2 == vector_lookup(&v1, 3));
+	
+	for (int i = 1; i < 4; i++) {
+                assert(i == vector_lookup(&v1, i));
+        }
 
-	for (int i = 3; i < 9; ++i) {
+
+	for (int i = 4; i < 9; ++i) {
 		assert(i-1 == vector_lookup(&v1, i));
 	}
+
 	for (int i = 9; i < len; ++i) {
 		assert(i == vector_lookup(&v1, i));
 	}
+
+	printf("---------------\n");
+	printf("----THE END----\n");
+	printf("---------------\n");
 
 	vector_destroy(&v1);
 
